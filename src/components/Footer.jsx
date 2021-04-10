2f89ae162e4a8b1ec65d641efd0e060a36be210c
Israil <isk75598@gmail.com>
2021-04-10 11:23:47 +0530

    My first react application

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>&copy; 2021 israil.com</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/emojipedia.js
@@ -0,0 +1,64 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🤓",
+      name: "Nerd Face",
+      meaning:
+        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+      id: 5,
+      emoji: "😭",
+      name: "Loudly Crying",
+      meaning: "Loudly Crying Face emoji is a smiley with closed (tightly or not) 👀 Eyes and streams of tears, 🏃 Running out of them."
+    },
+    {
+      id: 6,
+      emoji: "😆",
+      name: "Grinning Squinting Face",
+      meaning: "A yellow face with a broad, open smile and scrunched, X-shaped eyes. Often conveys excitement or hearty laughter."
+    },
+    {
+      id: 7,
+      emoji: " 😐",
+      name:"Neutral Face",
+      meaning:"A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor."
+    }
+    ,
+    {
+      id: 8,
+      emoji: " 😗",
+      name:"Kissing Face",
+      meaning:"A yellow face with simple, open eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+    },
+    {
+      id: 9,
+      emoji: " 😕",
+      name:"Confused Face",
+      meaning:"A yellow face with open eyes and a skewed frown, as if scrunching its cheeks or chewing its lips. A look of feeling unsure. An Google’s design heightens its intended puzzlement by including contorted eyebrows."
+    }
+  ];
+  
+  export default emojipedia;
+  
